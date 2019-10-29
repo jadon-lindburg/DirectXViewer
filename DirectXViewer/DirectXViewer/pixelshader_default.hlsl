175b2e586e5a6045b38f27efcc9856af1eb305de
Jadon Lindburg <jdlindburg@student.fullsail.edu>
2019-10-29 09:20:06 -0400

    Fix settings for x64, add data to shader inputs/outputs, add body of vertex shader

--- DirectXViewer/DirectXViewer/pixelshader_default.hlsl
+++ DirectXViewer/DirectXViewer/pixelshader_default.hlsl
@@ -9,17 +9,24 @@ cbuffer ConstantBuffer : register(b1)
 
 struct VSout
 {
-
+    float4 pos : SV_POSITION;
+    float4 norm : NORMAL;
+    float4 color : COLOR;
+    float2 uv : TEXCOORD;
+    float4 world_pos : WORLDPOS;
+    float4 eye_pos : EYEPOS;
 };
 
 struct PSout
 {
-
+    float4 color : SV_TARGET;
 };
 
 PSout main(VSout input)
 {
 	PSout output;
+
+    output.color = float4(1.0f, 1.0f, 1.0f, 0.0f);
 
 	return output;
 }
--- DirectXViewer/DirectXViewer/vertexshader_default.hlsl
+++ DirectXViewer/DirectXViewer/vertexshader_default.hlsl
@@ -9,17 +9,41 @@ cbuffer ConstantBuffer : register(b0)
 
 struct VSin
 {
-
+    float3 pos : POSITION;
+    float3 norm : NORMAL;
+    float4 color : COLOR;
+    float2 uv : TEXCOORD;
 };
 
 struct VSout
 {
-
+    float4 pos : SV_POSITION;
+    float4 norm : NORMAL;
+    float4 color : COLOR;
+    float2 uv : TEXCOORD;
+    float4 world_pos : WORLDPOS;
+    float4 eye_pos : EYEPOS;
 };
 
 VSout main(VSin input)
 {
 	VSout output;
+
+    output.world_pos = mul(float4(input.pos, 1.0f), world);
+
+    output.pos = mul(output.world_pos, view);
+    output.pos = mul(output.pos, projection);
+
+    output.eye_pos.x = -dot(view[3].xyz, view[0].xyz);
+    output.eye_pos.y = -dot(view[3].xyz, view[1].xyz);
+    output.eye_pos.z = -dot(view[3].xyz, view[2].xyz);
+    output.eye_pos.w = 1.0f;
+
+    output.norm = mul(float4(input.norm, 0.0f), world);
+
+    output.color = input.color;
+
+    output.uv = input.uv;
 
 	return output;
 }
